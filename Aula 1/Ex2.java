66e588286dbcbe309a3437a1ad8bcfff83565409
kaiqpaiva <kaique.paiva681@gmail.com>
2025-02-21 23:57:37 -0300

    git

---
+++ Aula 1/Ex2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+
+        System.out.print("Digite uma letra: ");
+        char letra = scanner.next().toLowerCase().charAt(0);
+
+    }
+}
