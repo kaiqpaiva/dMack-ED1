0046d83eb984e182e5cccc5bfb9730df806475a3
kaiqpaiva <kaique.paiva681@gmail.com>
2025-02-27 19:24:00 -0300

    ex 2 final

--- Aula 1/Ex2.java
+++ Aula 1/Ex2.java
@@ -7,5 +7,11 @@ public class Ex2 {
         System.out.print("Digite uma letra: ");
         char letra = scanner.next().toLowerCase().charAt(0);
 
+        if("aeiou".indexOf(letra) != -1){
+            System.out.println("Vogal");
+        } else {
+            System.out.println("Nao eh vogal");
+        }
+        scanner.close();
     }
 }
