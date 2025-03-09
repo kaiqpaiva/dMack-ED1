dc87740ecab7401cd0845713cdf0f36bee5523b2
kaiqpaiva <kaique.paiva681@gmail.com>
2025-03-08 23:53:26 -0300

    at de att

--- Aula 1/Ex7.java
+++ Aula 1/Ex7.java
@@ -1,5 +1,21 @@
 import java.util.Scanner;
 
 public class Ex7 {
-    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        int cont = 0;
+
+        char[] caracteres = text.toCharArray();
+
+        for(char c : caracteres){
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+                cont++;
+            }
+        }
+        System.out.println(cont);
+        scanner.close();
+    }
 }
